63629e4a9e7a6d9859b9ebddc9b4a57f287fdfc1
JustynaStebel <justyna.stebel@gmail.com>
2016-10-28 19:06:59 +0200

    Remove Admin as a separate model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :admins
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :users, only: [:show, :edit, :update, :destroy]
   root "users#show"
   resources :documents do
