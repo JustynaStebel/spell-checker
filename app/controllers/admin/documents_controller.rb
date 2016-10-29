84d85f3ded29aef0fa982b19eabf5786cc8284f0
JustynaStebel <justyna.stebel@gmail.com>
2016-10-29 10:27:06 +0200

    Add role admin

--- app/controllers/admin/documents_controller.rb
+++ app/controllers/admin/documents_controller.rb
@@ -1,2 +1,53 @@
 class Admin::DocumentsController < ApplicationController
+# before_action :authenticate_admin!
+def index
+  @documents = Document.all
+end
+
+def show
+  @document = Document.find(params[:id])
+  @document_attachments = @document.document_attachments.all
+end
+
+def new
+  @document = Document.new
+  # @document_attachment = @document.document_attachments.build
+end
+
+def create
+ @document = Document.create(document_params)
+
+ respond_to do |format|
+   if @document.save
+     params[:document_attachments]['file'].each do |f|
+        @document_attachment = @document.document_attachments.create!(:file => f)
+     end
+     format.html { redirect_to @document, notice: 'Document was successfully created.' }
+   else
+     format.html { render action: 'new' }
+   end
+ end
+end
+
+def edit
+  @document = Document.find(params[:id])
+end
+
+def update
+  @document = Document.find(params[:id])
+  @document.update!(document_params)
+  redirect_to document
+end
+
+def destroy
+  @document = Document.find(params[:id])
+  @document.destroy
+  redirect_to documents_path
+end
+
+
+private
+ def document_params
+    params.require(:document).permit(:name, document_attachments_attributes: [:id, :document_id, :file])
+ end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  private
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.admin == true
+      admin_users_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
