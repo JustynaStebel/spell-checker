ec72bfb54a457834db52baf8b3a3c47c00c9f0e8
JustynaStebel <justyna.stebel@gmail.com>
2016-11-08 22:01:16 +0100

    Add dependency in correct place

--- app/models/document_attachment.rb
+++ app/models/document_attachment.rb
@@ -1,5 +1,5 @@
 class DocumentAttachment < ApplicationRecord
   belongs_to :document
-  has_many :incorrect_words
+  has_many :incorrect_words, dependent: :destroy
   mount_uploader :file, FileUploader
 end
