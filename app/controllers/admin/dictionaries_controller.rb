5913af5c48d76a2b0729bf12e424f9ed10537c93
JustynaStebel <justyna.stebel@gmail.com>
2016-11-08 21:28:16 +0100

    Fix pagination

--- app/controllers/admin/dictionaries_controller.rb
+++ app/controllers/admin/dictionaries_controller.rb
@@ -2,6 +2,7 @@ class Admin::DictionariesController < ApplicationController
   require "will_paginate/array"
   def show
     @dictionary = Dictionary.last
-    @words  = @dictionary.words.paginate(:page => params[:page], :per_page => 50)
+    @words = @dictionary.words.flatten
+    @words  = @words.paginate(:page => params[:page], :per_page => 50)
   end
 end
