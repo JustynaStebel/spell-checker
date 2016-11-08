1e8c7eb4cfea302d58531daa6be60f5c47a92e38
JustynaStebel <justyna.stebel@gmail.com>
2016-11-08 21:46:09 +0100

    Add dependent destroy

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ApplicationRecord
   belongs_to :user
   has_many :document_attachments, dependent: :destroy
+  has_many :incorrect_words, dependent: :destroy
   accepts_nested_attributes_for :document_attachments
 end
