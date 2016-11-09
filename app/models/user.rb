c49e8c11a8f70d90612dd57dc7cbf187b5bf8dc7
JustynaStebel <justyna.stebel@gmail.com>
2016-11-09 08:53:29 +0100

    Add dependent destroy to user-documents

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :documents
+  has_many :documents, dependent: :destroy
 
   scope :without_admin, -> { where(admin: false) }
 end
